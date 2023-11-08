fb4d3c1ca89ddd8fc10cfcacd21886e590a0b69b
darko <ki14.skakun@gmail.com>
2023-11-08 16:25:03 +0200

    added starting home page

--- meetups/views.py
+++ meetups/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    meetups = [
+        {'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
+        {'title': 'A Second Meetup', 'location': 'Paris', 'slug': 'a-second-meetup'}
+    ]
+    context = {'meetups': meetups}
+    return render(request, 'meetups/index.html', context)
