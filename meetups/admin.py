44b2e9f5e2ba6e7a6e35149369aab2f541fd7ca0
darko <ki14.skakun@gmail.com>
2023-11-10 10:20:52 +0200

    added participang db model

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Meetup, Location
+from .models import Meetup, Location, Participant
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
